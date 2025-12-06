2d7d854ebd8975ba35aafabd77cc5025d1d52485
karthikeyan <akarthikeyan9876@gmail.com>
2025-12-06 16:21:53 +0530

    Migrated some fields and added some templates

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Blog
 
-# Create your views here.
+def posts_by_category(request, category_id):
+    # Fetch The Posts that belongs to the category with the category_id
+    posts = Blog.objects.filter(status='Published', category=category_id)
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts_by_category.html', context)
