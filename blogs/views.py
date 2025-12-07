64eeeaf63cbb4922b99d2d8d546946cda5b45f67
karthikeyan <akarthikeyan9876@gmail.com>
2025-12-07 12:19:47 +0530

    Added post_by_category

--- blogs/views.py
+++ blogs/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Blog
+from django.shortcuts import render, get_object_or_404
+from blogs.models import Blog, Category
+
 
 def posts_by_category(request, category_id):
-    # Fetch The Posts that belongs to the category with the category_id
     posts = Blog.objects.filter(status='Published', category=category_id)
+    category = get_object_or_404(Category, pk=category_id)
+
+    featured_post = Blog.objects.filter(status='Published').order_by('-created_at')[:1]
+
     context = {
         'posts': posts,
+        'category': category,
+        'featured_post': featured_post,
+        
     }
+
     return render(request, 'posts_by_category.html', context)
