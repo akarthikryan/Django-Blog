2a961867a33e117d3d19f36b139e485359bf86a9
karthikeyan <akarthikeyan9876@gmail.com>
2025-12-11 19:33:49 +0530

    Completed Posts CRUD Functionalities

--- dashboards/views.py
+++ dashboards/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from blogs.models import Category, Blog
 from django.contrib.auth.decorators import login_required
 from .forms import CategoryForm, BlogPostForm
-
+from django.utils.text import slugify
 
 @login_required(login_url='login')
 def dashboard(request):
@@ -81,3 +81,29 @@ def add_post(request):
     }
 
     return render(request, 'dashboard/add_post.html', context)
+
+
+def edit_post(request, pk):
+    post = get_object_or_404(Blog, pk=pk)
+    if request.method == 'POST':
+        form = BlogPostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            post = form.save()
+            title = form.cleaned_data['title']
+            post.slug = slugify(title) + '-' + str(post.id)
+            post.save()
+            return redirect('posts')
+        else:
+            print('Form is invalid:')
+            print(form.errors)
+    form = BlogPostForm(instance=post)
+    context = {
+        'form': form,
+        'post': post,
+    }
+    return render(request, 'dashboard/edit_post.html', context)
+
+def delete_post(request, pk):
+    post = get_object_or_404(Blog, pk=pk)
+    post.delete()
+    return redirect('posts')
