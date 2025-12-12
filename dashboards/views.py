b59af2ff22230e6864cb186fe0dd745c297e3c16
karthikeyan <akarthikeyan9876@gmail.com>
2025-12-12 13:41:12 +0530

    Added Some User Functionalities

--- dashboards/views.py
+++ dashboards/views.py
@@ -3,6 +3,8 @@ from blogs.models import Category, Blog
 from django.contrib.auth.decorators import login_required
 from .forms import CategoryForm, BlogPostForm
 from django.utils.text import slugify
+from django.contrib.auth.models import User
+from .forms import AddUserForm
 
 @login_required(login_url='login')
 def dashboard(request):
@@ -107,3 +109,27 @@ def delete_post(request, pk):
     post = get_object_or_404(Blog, pk=pk)
     post.delete()
     return redirect('posts')
+
+
+# -------------------- Users Views --------------------
+def users(request):
+    users = User.objects.all()
+    context = {
+        'users': users,
+    }
+    return render(request, 'dashboard/users.html', context)
+
+
+def add_user(request):
+    if request.method == 'POST':
+        form = AddUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('users')
+        else:
+            print(form.errors)
+    form = AddUserForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'dashboard/add_user.html', context)
